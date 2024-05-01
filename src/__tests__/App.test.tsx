d94d9d936ef923a79a218dfeee688d94b8f89b96
Egor <egor.krasnonosenkikh.mil@swf.army.mil>
2024-04-30 22:34:25 -0400

    Complete test aircraft form test

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,6 +1,10 @@
 import { describe, expect, test } from 'vitest'
-import { render, screen, within } from '@testing-library/react'
 import App from '../App'
+import { render, screen, within } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
+import { server } from '../mocks/node'
+import { HttpResponse, http } from 'msw'
+import { Aircraft } from '../types/Aircraft'
 
 describe('when the app is rendered', () => {
     test('should display header', () => {
@@ -16,5 +20,33 @@ describe('when the app is rendered', () => {
 
         expect(within(listElements[0]).getByText('Monoplane'))
         expect(within(listElements[0]).getByText('Egor'))
+    })
+
+    test('should add aircraft to list on succesfull post', async () => {
+        server.use(
+            http.post('/api/aircraft', async ({ request }) => {
+                const requestData = (await request.json()) as Aircraft
+                return HttpResponse.json({ ...requestData, id: 2 })
+            })
+        )
+
+        render(<App />)
+
+        const airframe = 'Ornithopter'
+        const pilot = 'Paul'
+
+        const airframeInput = screen.getByLabelText('Airframe')
+        const pilotInput = screen.getByLabelText('Pilot')
+        const submitButton = screen.getByRole('button', { name: /create/i })
+
+        await userEvent.type(airframeInput, airframe)
+        await userEvent.type(pilotInput, pilot)
+        await userEvent.click(submitButton)
+
+        const listElements = await screen.findAllByRole('listitem')
+        expect(listElements).toHaveLength(2)
+
+        expect(within(listElements[1]).getByText(airframe))
+        expect(within(listElements[1]).getByText(pilot))
     })
 })
