10db49b07c305b885b4a173f960f4c95566f1252
Egor K <egor.krasnonosenkikh.mil@swf.army.mil>
2024-05-02 17:27:59 -0500

    Add react-hook-forms and zod integrations

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import { FormEvent, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import AircraftCard from './AircraftCard.tsx'
 import { Aircraft } from './types/Aircraft.ts'
+import { AlertCircleIcon } from 'lucide-react'
+import { useForm } from 'react-hook-form'
+import { ZodType, z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+type AircraftFormTypes = {
+    airframe: string
+    pilot: string
+}
+
+const AircraftSchema: ZodType<AircraftFormTypes> = z.object({
+    airframe: z.string().min(2).max(25),
+    pilot: z.string().min(2).max(25),
+})
 
 function App() {
     const [data, setData] = useState<Aircraft[]>([])
 
-    const [airframe, setAirframe] = useState('')
-    const [pilot, setPilot] = useState('')
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<AircraftFormTypes>({
+        resolver: zodResolver(AircraftSchema),
+    })
 
     useEffect(() => {
         const response = async () => {
@@ -18,27 +37,17 @@ function App() {
         response()
     }, [])
 
-    const onFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-
-        const newAircraft = {
-            airframe,
-            pilot,
-        }
-
+    const onFormSubmit = async (values: AircraftFormTypes) => {
         const response = await fetch('/api/aircraft', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(newAircraft),
+            body: JSON.stringify(values),
         })
         const responseData = await response.json()
 
         setData([...data, responseData])
-
-        setAirframe('')
-        setPilot('')
     }
 
     return (
@@ -48,29 +57,53 @@ function App() {
 
                 <form
                     className="mb-8 flex flex-col gap-4"
-                    onSubmit={onFormSubmit}
+                    onSubmit={handleSubmit(onFormSubmit)}
                 >
                     <div className="flex flex-col">
                         <label className="text-sm" htmlFor="airframe">
                             Airframe
                         </label>
                         <input
+                            {...register('airframe', {
+                                required: true,
+                                maxLength: 20,
+                                minLength: 2,
+                            })}
                             className=" border border-gray-200 p-2 rounded-md mt-0.5"
                             id="airframe"
-                            value={airframe}
-                            onChange={(e) => setAirframe(e.target.value)}
                         />
+                        {errors.airframe?.message && (
+                            <div className="flex items-center gap-1 mt-1">
+                                <AlertCircleIcon
+                                    size={18}
+                                    className="fill-red-500 stroke-white"
+                                />
+                                <span className="text-red-500 text-sm">
+                                    {errors.airframe?.message}
+                                </span>
+                            </div>
+                        )}
                     </div>
                     <div className="flex flex-col">
                         <label className="text-sm" htmlFor="pilot">
                             Pilot
                         </label>
                         <input
+                            {...register('pilot', { required: true })}
                             className=" border border-gray-200 p-2 rounded-md mt-0.5"
                             id="pilot"
-                            value={pilot}
-                            onChange={(e) => setPilot(e.target.value)}
                         />
+                        {errors.pilot?.message && (
+                            <div className="flex items-center gap-1 mt-1">
+                                <AlertCircleIcon
+                                    size={18}
+                                    className="fill-red-500 stroke-white"
+                                />
+                                <span className="text-red-500 text-sm">
+                                    {errors.pilot?.message}
+                                </span>
+                            </div>
+                        )}
                     </div>
 
                     <button
